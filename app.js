8cf1793d9e49bc6f5e09562b55395d0c875dee0e
Serhat GECGIN <74310970+gecginserhat@users.noreply.github.com>
2024-09-03 22:53:33 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,16 +1,3 @@
-console.log("Hello JavaScript");
-console.log(213213);
-console.log(true);
-console.log(false);
-
-var a = 20;
-
-console.log(a);
-
-console.log(["Ahmet","Mehmet"]);
-
-console.log(typeof a);
-
-// console.warn("This is warning");
-// console.error("This page is not found");
-// console.clear();
+alert("Welcome to the JavaScript");
+var a = 10;
+alert(a);
